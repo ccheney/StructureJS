7c15a323c8354de33e0c7446a3fc7e16d9324e3d
codeBelt <code@codebelt.com>
2015-11-04 22:43:50 -0600

    Update SinglePageWebsite

---
+++ examples/SinglePageWebsite-RequireJS/src/assets/scripts/main.js
@@ -0,0 +1,26 @@
+require(
+    // The only purpose of this file is to kick off your application's top-level
+    // controller at the appropriate time. All other code should be written as
+    // separate modules in their own files.
+    //
+    // Note that since this is the application entry-point, traditional
+    // RequireJS syntax is used here to specify dependencies. Do not use this
+    // syntax in any other modules.
+    [
+        'jquery',
+        './WebsiteApp'
+    ],
+    function(
+        $,
+        WebsiteApp
+    ) {
+        'use strict';
+        $(document).ready(function () {
+
+            window.app = new WebsiteApp();
+            window.app.appendTo('body');    // Need to specify what area our code has control over.
+                                            // The WebsiteApp.js class extends Stage which has the appendTo method.
+                                            // Note: On typical website you may want to set it as 'body' do you have control over the whole page.
+        });
+    }
+);
