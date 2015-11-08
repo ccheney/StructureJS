67ad0d91133a649969a8cdb6b3a7de876c6a70a8
Chris Cheney <ccheney05@gmail.com>
2015-11-07 22:51:06 -0600

    update deps

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config)
         plugins: [
             'karma-phantomjs-launcher',
             'karma-jasmine',
-            'karma-bro'
+            'karma-browserify'
         ],
 
 
