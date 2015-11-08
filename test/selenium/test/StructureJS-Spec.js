77ff07c73f971e2a6401589b6cdc78e299182bcd
Chris Cheney <ccheney05@gmail.com>
2015-11-08 04:31:21 -0600

    null check

--- test/selenium/test/StructureJS-Spec.js
+++ test/selenium/test/StructureJS-Spec.js
@@ -6,7 +6,7 @@ var webdriver = require('selenium-webdriver');
 
 describe('testing javascript in the browser', function() {
     beforeEach(function() {
-        if (process.env.SAUCE_USERNAME != undefined) {
+        if (process.env.SAUCE_USERNAME != null) {
             this.browser = new webdriver.Builder()
                 .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
                 .withCapabilities({
